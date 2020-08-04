da2c763617bb5f5c4183f1972860a81c22c3cf46
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-08-04 15:32:20 +0700

    remove default files from CRA

--- finish/src/index.tsx
+++ finish/src/index.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 
 import App from './App';
 import { customTheme } from './theme'
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <ThemeProvider theme={customTheme}>
@@ -15,8 +14,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
