db8d861f0523197593d141efcfda76d6ca13759f
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-08-04 15:26:36 +0700

    first try Chakra UI

--- finish/src/App.tsx
+++ finish/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Badge, Stack } from '@chakra-ui/core'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Stack spacing={8} isInline align="center">
+      <Badge rounded={10}>Default</Badge>
+      <Badge>Default</Badge>
+      <Badge>Default</Badge>
+      <Badge>Default</Badge>
+      <Badge>Default</Badge>
+    </Stack>
+  )
 }
 
-export default App;
+export default App
