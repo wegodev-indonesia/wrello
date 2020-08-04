1306df1f74c4153eeb35778c61ca173162bd76eb
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-08-04 13:50:31 +0700

    add, implement, windmill ui

--- finish/src/index.tsx
+++ finish/src/index.tsx
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Windmill } from '@windmill/react-ui'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Windmill>
+      <App />
+    </Windmill>
   </React.StrictMode>,
   document.getElementById('root')
 );
