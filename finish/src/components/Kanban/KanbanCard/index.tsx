e02223594b3ed57bdc8fc829da23d043dab80239
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-08-08 09:37:17 +0700

    Droppable KanbanCard

--- finish/src/components/Kanban/KanbanCard/index.tsx
+++ finish/src/components/Kanban/KanbanCard/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Droppable } from "react-beautiful-dnd";
 import { Box, Text, Flex, VStack } from "@chakra-ui/core";
 
+import { Task } from "data/initial-data";
 import TaskCard from "components/Kanban/TaskCard";
 
 type KanbanCardProps = {
+  id: string;
   title: string;
+  tasks: Task[];
 };
 
-const KanbanCard: React.FC<KanbanCardProps> = ({ title }) => {
+const KanbanCard: React.FC<KanbanCardProps> = ({ id, title, tasks = [] }) => {
   return (
     <Box minWidth="270px" p={2} bg="gray.300" rounded={4}>
       <Flex direction="column">
@@ -17,11 +21,28 @@ const KanbanCard: React.FC<KanbanCardProps> = ({ title }) => {
           </Text>
         </Box>
 
-        <VStack flex={1} minHeight="60px">
-          <TaskCard task="Coding" />
-          <TaskCard task="Coding" />
-          <TaskCard task="Coding" />
-        </VStack>
+        <Droppable droppableId={id}>
+          {(provided, snapshot) => (
+            <VStack
+              flex={1}
+              minHeight="60px"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {tasks.length > 0
+                ? tasks.map((task, index) => (
+                    <TaskCard
+                      key={task.id}
+                      content={task.content}
+                      id={task.id}
+                      index={index}
+                    />
+                  ))
+                : null}
+              {provided.placeholder}
+            </VStack>
+          )}
+        </Droppable>
       </Flex>
     </Box>
   );
