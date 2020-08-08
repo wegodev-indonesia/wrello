7a8af6f0f921785eeb625a0dcddb2b42a4ea008d
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-08-08 09:37:01 +0700

    DragDropContext KanbanStack

--- finish/src/components/Kanban/KanbanStack/index.tsx
+++ finish/src/components/Kanban/KanbanStack/index.tsx
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { HStack } from "@chakra-ui/core";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
+import { initialData, InitialData } from "data/initial-data";
 import KanbanCard from "components/Kanban/KanbanCard";
 
 const KanbanStack: React.FC = () => {
+  const [data, setData] = useState<InitialData>(initialData);
+
+  const onDragEnd = (result: DropResult): void => {
+    // const { destination, source, draggableId } = result;
+  };
+
   return (
-    <HStack
-      mt="40px"
-      p={4}
-      spacing={8}
-      top={0}
-      right={0}
-      bottom={0}
-      left={0}
-      userSelect="none"
-      whiteSpace="nowrap"
-      overflowX="auto"
-      overflowY="hidden"
-      position="absolute"
-      alignItems="flex-start"
-    >
-      <KanbanCard title="Todo" />
-      <KanbanCard title="In Progress" />
-      <KanbanCard title="Done" />
-    </HStack>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <HStack
+        mt="40px"
+        p={4}
+        spacing={8}
+        top={0}
+        right={0}
+        bottom={0}
+        left={0}
+        userSelect="none"
+        whiteSpace="nowrap"
+        overflowX="auto"
+        overflowY="hidden"
+        position="absolute"
+        alignItems="flex-start"
+      >
+        {data.columnOrder.map(
+          (columnId: string): React.ReactNode => {
+            const column = data.columns[columnId];
+            const tasks = (column.taskIds as string[]).map(
+              (taskId: string) => data.tasks[taskId]
+            );
+
+            return (
+              <KanbanCard
+                key={column.id}
+                id={column.id}
+                title={column.title}
+                tasks={tasks}
+              />
+            );
+          }
+        )}
+      </HStack>
+    </DragDropContext>
   );
 };
 
